e4d884803a3ecd31a34b3ca2ecee29f8664b2324
aaron.hoffer <aaron.hoffer@connexta.com>
2020-12-02 11:35:08 -0700

    First commit

---
+++ day_1_repair-report/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to day_1_repair-report
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ day_1_repair-report/test/day_1_repair_report/core_test.clj
@@ -0,0 +1,7 @@
+(ns day-1-repair-report.core-test
+  (:require [clojure.test :refer :all]
+            [day-1-repair-report.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
