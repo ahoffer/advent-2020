17f9c847eb6c1cf80f28a223f663ee9e53da6ee2
aaron.hoffer <aaron.hoffer@connexta.com>
2020-12-07 12:10:16 -0700

    Happy path works

--- day_1_repair-report/src/day_1_repair_report/core.clj
+++ day_1_repair-report/src/day_1_repair_report/core.clj
@@ -1,6 +1,28 @@
 (ns day-1-repair-report.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn check
+  [a b]
+  (= (+ a b) 2020))
+
+(defn checklist
+  [a list]
+  (if (empty? list)
+    nil
+    (if (check a (first list))
+      (first list)
+      (checklist a (drop 1 list)))))
+
+(def l '(2019 2020 2021))
+
+(defn checklists
+  [list1 list2]
+  (if (empty? list1)
+    nil
+    (let [addend (first list1)
+          answer (checklist addend list2)]
+      (println answer)
+      (if answer
+        [addend answer]
+        (checklists (drop 1 list1) list2)))))
+
+(def l2 '(2 3 1))
