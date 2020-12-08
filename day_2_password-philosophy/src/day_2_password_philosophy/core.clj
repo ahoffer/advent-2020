a0881235886ed06ea0a0a8ff8343608fe15f726e
aaron.hoffer <aaron.hoffer@connexta.com>
2020-12-08 09:10:07 -0700

    input reader works

--- day_2_password-philosophy/src/day_2_password_philosophy/core.clj
+++ day_2_password-philosophy/src/day_2_password_philosophy/core.clj
@@ -4,3 +4,19 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def regexpr #"(\d)+-(\d)+ (\w): (\w+)")
+
+(defn read-input
+  [input]
+  (map #(rest (re-matches regexpr %)) input))
+
+(def input (clojure.string/split-lines (slurp "resources/input.txt")))
+
+(read-input "1-2 a: ddfadfa")
+(read-input "4-6 n: trwpnnnvq\n12-15 p: zfpmpphpgghpppppppp")
+(read-input input)
+
+(def numbers (let [file-contents (slurp "resources/input.txt")
+                   nums-as-strings (clojure.string/split file-contents #"\n")]
+               (map read-string nums-as-strings)))
