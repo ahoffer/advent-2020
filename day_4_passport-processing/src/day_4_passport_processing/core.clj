8ffb2e23160957d7a4566898f71c7a994a63280f
aaron.hoffer <aaron.hoffer@connexta.com>
2020-12-09 13:25:15 -0700

    Day 4 done

--- day_4_passport-processing/src/day_4_passport_processing/core.clj
+++ day_4_passport-processing/src/day_4_passport_processing/core.clj
@@ -5,35 +5,6 @@
               lines (clojure.string/split-lines file-contents)]
              lines))
 
-;(defn get-documents
-;  [lines]
-;  (loop [remaining-lines lines
-;         document ""
-;         documents []]
-;    (if (empty? remaining-lines)
-;
-;      ;Done. Return results.
-;      (if (empty? document)
-;        documents
-;        (conj documents document))
-;
-;      ;There are still more lines of data to process
-;      (let [line (first remaining-lines)]
-;
-;        (if (not-empty line)
-;
-;          ;The document continues on this line. Append the line and keep going.
-;          (recur
-;            (rest lines)
-;            (clojure.string/trim (str (clojure.string/trim document) " " line))
-;            documents)
-;
-;          ;We hit the document separator. Add the current doc to collection and keep going.
-;          (recur
-;            (drop-while clojure.string/blank? remaining-lines)
-;            ""
-;            (conj documents document)))))))
-
 (defn consume-next-doc
   [lines]
   (let
@@ -68,3 +39,54 @@
 
 (def document-data (map doc-to-map documents))
 
+(defn north-pole-cred?
+  [datum]
+  (contains? datum "cid"))
+
+(defn contains-expected?
+  [datum]
+  (let [fields '("byr"
+                  "iyr"
+                  "eyr"
+                  "hgt"
+                  "hcl"
+                  "ecl"
+                  "pid"
+                  )]
+    (every? #(contains? datum %) fields)))
+
+;PART TWO
+;byr (Birth Year) - four digits; at least 1920 and at most 2002.
+;iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+;eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+;hgt (Height) - a number followed by either cm or in:
+;If cm, the number must be at least 150 and at most 193.
+;If in, the number must be at least 59 and at most 76.
+;hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+;ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+;pid (Passport ID) - a nine-digit number, including leading zeroes.
+;cid (Country ID) - ignored, missing or not.
+
+(defn valid-height?
+  [hgt]
+  (let [[match num unit] (re-find #"^(\d+)(in|cm)$" hgt)]
+    (and (not (nil? match))
+         (let
+           [value (Integer/parseInt num)]
+           (if (= unit "cm")
+             (<= 150 value 193)
+             (<= 59 value 76))))))
+
+(defn valid?
+  [datum]
+  (and (contains-expected? datum)
+       (<= 1920 (Integer/parseInt (datum "byr")) 2002)
+       (<= 2010 (Integer/parseInt (datum "iyr")) 2020)
+       (<= 2020 (Integer/parseInt (datum "eyr")) 2030)
+       (valid-height? (datum "hgt"))
+       (not (nil? (re-find #"^#[a-z0-9]{6}$" (datum "hcl"))))
+       (not (nil? (re-find #"^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$" (datum "ecl"))))
+       (not (nil? (re-find #"^\d{9}$" (datum "pid"))))))
+
+;(count (filter valid? document-data))
+;(valid? {"hcl" "#7d3b0c", "pid" "431742871", "ecl" "hzl", "hgt" "169cm", "cid" "340", "eyr" "2023", "iyr" "2017", "byr" "1994"})
