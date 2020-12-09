75f5b9e0ca137c251e6f1e710ebff220a5f44498
aaron.hoffer <aaron.hoffer@connexta.com>
2020-12-08 21:26:46 -0700

    Completed both parts

--- day_3_toboggan-trajectory/src/day_3_toboggan_trajectory/core.clj
+++ day_3_toboggan-trajectory/src/day_3_toboggan_trajectory/core.clj
@@ -1,6 +1,103 @@
 (ns day-3-toboggan-trajectory.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:dynamic *verbose* false)
+
+(defmacro printfv
+  [fmt & args]
+  `(when *verbose*
+     (printf ~fmt ~@args)))
+
+(defmacro with-verbose
+  [& body]
+  `(binding [*verbose* true] ~@body))
+
+;--- Day 3: Toboggan Trajectory ---
+;With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy,
+; it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which
+; angles will take you near the fewest trees.
+;
+;Due to the local geology, trees in this area only grow on exact integer coordinates in a grid.
+; You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+(def test1 (clojure.string/split-lines "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"))
+
+(def test2 (clojure.string/split-lines "###
+###
+###"))
+
+;These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability,
+; the same pattern repeats to the right many times:
+
+; You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+; The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers);
+; start by counting all the trees you would encounter for the slope right 3, down 1:
+; From your starting position at the top-left, check the position that is right 3 and down 1.
+; Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+; The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+; In this example, traversing the map using this slope would cause you to encounter 7 trees.
+; Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+(def input (clojure.string/split-lines (slurp "resources/input.txt")))
+
+;(println input)
+;(println (count input) " lines")
+
+(defn replace-char
+  [s index new-char]
+  (str (subs s 0 index) new-char (subs s (inc index))))
+
+(defn grid-value
+  [x y data]
+  (let [
+        num-cols (count (first data))
+        x (mod x num-cols)
+        line (get data y)
+        character (get line x)
+        value (if (= character \.) :open :tree)
+        symbol (if (= value :open) "O" "X")]
+    (println line "\t" (replace-char line x symbol))
+    value))
+
+(defn count-trees
+  [x y step-right step-down tree-count data]
+  (let
+    [next-x (+ x step-right)
+     next-y (+ y step-down)
+     obj (grid-value x y data)
+     tree-count (if (= obj :tree) (inc tree-count) tree-count)
+     num-rows (count data)]
+    (if (< next-y num-rows)
+      (count-trees next-x next-y step-right step-down tree-count data)
+      tree-count)))
+
+(count-trees 0 0 3 1 0 input)
+(println (= (count-trees 0 0 3 1 0 input) 278))
+
+;--- Part Two ---
+;Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+;
+;Determine the number of trees you would encounter if, for each of the following slopes,
+; you start at the top-left corner and traverse the map all the way to the bottom:
+;
+;Right 1, down 1.
+;Right 3, down 1. (This is the slope you already checked.)
+;Right 5, down 1.
+;Right 7, down 1.
+;Right 1, down 2.
+;In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together,
+;these produce the answer 336.
+;
+;What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+(reduce * (map #(count-trees 0 0 (first %) (second %) 0 input) '((1 1) (3 1) (5 1) (7 1) (1 2))))
+;Answer 9709761600
