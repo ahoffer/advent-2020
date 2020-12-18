63d450559865c03a075aa4d977c9cafe047cb50c
aaron.hoffer <aaron.hoffer@connexta.com>
2020-12-18 02:13:54 -0700

    Time to pull out bag names, look up their rules, and add them as children.

--- day_7_handy-haversacks/src/day_7_handy_haversacks/core.clj
+++ day_7_handy-haversacks/src/day_7_handy_haversacks/core.clj
@@ -1,6 +1,8 @@
 (ns day-7-handy-haversacks.core)
+(require '(clojure [zip :as zip]))
+(def z (zip/vector-zip [1 [:a :b] 2 3 [40 50 60]]))
 
-(def raw-sample-input
+(def input-1
   '("light red bags contain 1 bright white bag, 2 muted yellow bags."
      "dark orange bags contain 3 bright white bags, 4 muted yellow bags."
      "bright white bags contain 1 shiny gold bag."
@@ -9,39 +11,27 @@
      "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
      "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."
      "faded blue bags contain no other bags."
-     "dotted black bags contain no other bags."
-     ))
-;What would a rule look like?
-;A rule is a bag name that contains other bag names along with their cardinalities.
-;There needs to be a iterative-recursive function that iterates over a bags contents and recursively expands the contents.
+     "dotted black bags contain no other bags."))
 
-; ContentItem [cardinality, bagId], where bagId is a reference to a Bag
-; Bag [bagId, set of ContentItem]
-; Rules - the set of all Bags
-; function expand:
-;   input: bagId, Rules
-;   output: Map. Keys are bagId and values are how many of these bags are ultimately contained within.
 
-;Part 1 - How many bag colors can eventually contain at least one shiny gold bag?
-; Solution steps:
-; 1. Get all the keys from Rules. That should be every bagId in the domain
-; 2. Expand every bagId
-; 3. Save the expanded Bags as a map (ExpandedBags), keyed by bag ID.
-;   The values are output of the expand function.
-; 4. Filter ExpandedBags to get all the bagIds that contain a shiny-gold bag
+(def input-2 (map clojure.string/trim
+                  (clojure.string/split-lines
+                    "shiny gold bags contain 2 dark red bags.
+                   dark red bags contain 2 dark orange bags.
+                   dark orange bags contain 2 dark yellow bags.
+                   dark yellow bags contain 2 dark green bags.
+                   dark green bags contain 2 dark blue bags.
+                   dark blue bags contain 2 dark violet bags.
+                   dark violet bags contain no other bags.")))
 
-;Rule Structure
-;
-; bagId-1 bagId-2 " bags contain " (cardinality-A contentId-A1 contentId-A2 " bag/s, ")* cardinality-B contentId-B1 contentId-B2 " bag/s."
-;
-
-;Example
-;(make-bagId "faded blue")
-;=> :faded-blue
+; Example
+; (make-bagId "faded blue")
+; => :faded-blue
 (defn make-keyword
   "Convert string to keyword 'faded blue' -> :faded-blue"
   [name]
   (keyword (clojure.string/replace name " " "-")))
+
 
 
 (defn extract-id
@@ -50,65 +40,104 @@
 
 
 
-;(extract-content-items one-raw-input)
-;=> {:bright-white 1, :muted-yellow 2}
-(defn extract-content-items
+; (content-tokens (first input-1))
+; => (("1" "bright white") ("2" "muted yellow"))
+(defn content-tokens
   [line]
-  (let [groups (re-seq #"(\d) (\w+ \w+)" line)
-        items (map rest groups)]
-    (reduce #(assoc %1 (make-keyword (second %2)) (Integer/parseInt (first %2))) {} items)))
+  (map rest (re-seq #"(\d) (\w+ \w+)" line)))
 
 
-;(make-bag one-raw-input)
-;=> {:light-red {:bright-white 1, :muted-yellow 2}}
-(defn make-bag
+
+; (make-content-nodes (first input-1))
+; => [[:bright-white 1] [:muted-yellow 2]]
+(defn make-content-nodes
   [line]
-  {(extract-id line) (extract-content-items line)})
+  (let [token-groups (content-tokens line)]
+    (reduce #(let [child-arity (Integer/parseInt (first %2))
+                   child-id (make-keyword (second %2))]
+               (conj %1 [child-id child-arity]))
+            []
+            token-groups)))
 
 
-;(make-rules raw-sample-input)
-;=>
-;{:vibrant-plum {:faded-blue 5, :dotted-black 6},
-; :light-red {:bright-white 1, :muted-yellow 2},
-; ...
-(defn make-rules
-  [input]
-  (apply merge (map make-bag input)))
 
-(def sample-rules (make-rules raw-sample-input))
+; (make-rule (first input-2))
+; => {:light-red [[:bright-white 1] [:muted-yellow 2]]}
+(defn make-rule
+  [line]
+  {(extract-id line) (make-content-nodes line)})
 
 
-;(test-one :shiny-gold sample-rules)
-;=> {:dark-olive 1, :vibrant-plum 2, :faded-blue 13, :dotted-black 16
-(defn expand-id
-  ([id rules] (expand-id id rules 1))
-  ([id rules cardinality]
-   (let [rule (id rules)]
-     (if (empty? rule)
-       {}
-       (let [contents (apply merge (map (fn [[k v]] {k (* cardinality v)}) rule))]
-         (reduce (fn [c bag-id] (merge-with + c (expand-id bag-id rules (bag-id rule)))) contents (keys rule)))))))
+(defn make-rules [lines]
+  (apply merge (map make-rule lines)))
+
+(def rules-1 (make-rules input-1))
+(def rules-2 (make-rules input-2))
+
+(defn print-loc [loc]
+  (if-not (zip/end? loc)
+    (println "loc " (zip/node loc)))
+  (recur (zip/next loc)))
 
 
-(def raw-input (clojure.string/split-lines (slurp "resources/input.txt")))
+; (make-tree (zip/vector-zip []) :faded-blue rules-1)
+; (make-tree (zip/vector-zip []) :vibrant-plum rules-2)
+(defn make-tree
+  ([id rules] (make-tree nil id rules))
+  ([loc id rules]
+   (let [item (rules id)]
+     (if-not loc (clojure.zip/vector-zip item))
 
-(def real-rules (make-rules raw-input))
+     )))
 
-(defn unwind
-  [rules]
-  (map (fn [k] {k (expand-id k rules)})
-       (keys rules)))
 
-(def unwound-real-rules (unwind real-rules))
 
-;(contains-particular-bag :shiny-gold (unwind sample-rules))
-(defn contains-particular-bag
-  [particular-bag-id unwound-rules]
-  (let []
-    (println (type (first unwound-rules)))
-    ;(filter
-    ;  #(contains? (% unwound-rules) particular-bag-id)
-    ;  (keys unwound-rules))
-    )
-  )
+(defn arity [zipped-rule]
+  (-> zipped-rule
+      zip/next
+      zip/next
+      zip/node))
+
+;(arity (zip/vector-zip [:light-red 1 [:bright-white 1 :muted-yellow 2]]))
+; => 1
+
+
+
+(defn bag-name [zipped-rule]
+  (-> zipped-rule
+      zip/next
+      zip/node))
+
+;(bag-name (zip/vector-zip [:light-red 1 [:bright-white 1 :muted-yellow 2]]))
+;=> :light-red
+
+
+
+(defn bag-contents [zipped-rule]
+  (-> zipped-rule
+      zip/down
+      zip/rightmost
+      zip/children))
+
+;(bag-contents (zip/vector-zip [:light-red 1 [:bright-white 1 :muted-yellow 2]]))
+; => (:bright-white 1 :muted-yellow 2)
+
+
+
+
+
+
+
+
+
+
+
+; ZIPPER EXPERIMENTS
+;
+;(-> [:light-red 1 [:bright-white 1 :muted-yellow 2]]
+;    zip/vector-zip
+;    zip/down
+;    zip/rightmost
+;    zip/children)
+;=> (:bright-white 1 :muted-yellow 2)
 
